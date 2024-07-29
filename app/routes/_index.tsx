40bc8595849e744c5ea7b21abbfb3e9cf64897f2
nomnel <laxyz.nomnel@gmail.com>
2024-07-29 23:59:31 +0900

    update header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className='font-sans p-4'>
-      <h1 className='text-3xl'>Welcome to Remix on Cloudflare Deployed from GitHub Actions</h1>
+      <h1 className='text-3xl'>Deployed from GitHub Actions</h1>
       <ul className='list-disc mt-4 pl-6 space-y-2'>
         <li>
           <a
